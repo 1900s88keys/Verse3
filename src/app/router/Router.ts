1397f62096a99fe3f4823db9f6e10727fad833a2
1900s88keys <rancong_hot@163.com>
2025-11-21 00:28:37 +0800

    更新路由

--- src/app/router/Router.ts
+++ src/app/router/Router.ts
@@ -1,30 +1,30 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: () => import("@/pages/home-page/HomePage.vue"),
+      path: '/',
+      name: 'home',
+      component: () => import('@/pages/home-page/HomePage.vue'),
       meta: {
-        title: "首页",
+        title: '首页',
       },
     },
     {
-      path: "/demo",
-      name: "demo",
-      component: () => import("@/pages/layout-demo/LayoutDemo.vue"),
+      path: '/demo',
+      name: 'demo',
+      component: () => import('@/pages/layout-demo/LayoutDemo.vue'),
       meta: {
-        title: "布局演示",
+        title: '布局演示',
       },
     },
     {
-      path: "/reflection",
-      name: "reflection",
-      component: () => import("@/pages/reflection-page/ReflectionPage.vue"),
+      path: '/global',
+      name: 'global',
+      component: () => import('@/pages/reflection-page/ReflectionPage.vue'),
       meta: {
-        title: "反射演示",
+        title: '地球',
       },
     },
   ],
